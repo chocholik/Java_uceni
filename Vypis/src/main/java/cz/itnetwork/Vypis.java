87f7bdd25021b544f77170d2543093b0db770934
Jan Chochole <chochole@Jans-MacBook-Pro.local>
2023-06-11 16:12:14 +0200

    Second program in InteliJ with variables

---
+++ Vypis/src/main/java/cz/itnetwork/Vypis.java
@@ -0,0 +1,12 @@
+package cz.itnetwork;
+
+public class Vypis {
+    public static void main(String[] args){
+        int a;
+        a = 56;
+        System.out.println(a);
+        double b;
+        b = 3.14;
+        System.out.println(b);
+    }
+}
